a0b5b751afe6bafec4b08cdedc52df9c4976a3ac
Jeje <jjh4450jjh4450@gmail.com>
2024-09-20 19:05:33 +0900

    init

---
+++ src/main/java/jeje/work/aeatbe/AeatbeApplication.java
@@ -0,0 +1,12 @@
+package jeje.work.aeatbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeatbeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AeatbeApplication.class, args);
+  }
+}
