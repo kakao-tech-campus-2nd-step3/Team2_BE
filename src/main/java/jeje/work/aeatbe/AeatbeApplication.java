a6bbc29d19651dd85ec975025aee2ec6d39693c7
youcalste03 <luckthb325@naver.com>
2024-10-02 18:28:36 +0900

    feat:JpaAuditing

--- src/main/java/jeje/work/aeatbe/AeatbeApplication.java
+++ src/main/java/jeje/work/aeatbe/AeatbeApplication.java
@@ -2,8 +2,10 @@ package jeje.work.aeatbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AeatbeApplication {
 
   public static void main(String[] args) {
