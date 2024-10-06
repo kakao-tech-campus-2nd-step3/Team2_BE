e93ec1d889b91c3d0c3efde35bacbbaa5d60fb3c
peacefullyquietly <pjh73015@gmail.com>
2024-10-06 22:34:18 +0900

    feat : 각 Entity에 대한 repository 클래스 작성

---
+++ src/main/java/jeje/work/aeatbe/repository/ProductAllergyRepository.java
@@ -0,0 +1,8 @@
+package jeje.work.aeatbe.repository;
+
+import jeje.work.aeatbe.entity.ProductAllergy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductAllergyRepository extends JpaRepository<ProductAllergy, Integer> {
+
+}
