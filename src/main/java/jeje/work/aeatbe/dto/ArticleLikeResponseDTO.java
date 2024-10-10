36f4de2e5bde0b69cf93f5aebc900c1da006f641
youcaslte03 <luckthb325@naver.com>
2024-10-10 20:13:08 +0900

    fix:git conflict해결

---
+++ src/main/java/jeje/work/aeatbe/dto/ArticleLikeResponseDTO.java
@@ -0,0 +1,14 @@
+package jeje.work.aeatbe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArticleLikeResponseDTO {
+
+    private Long articleLikeId;
+
+}
--- src/main/java/jeje/work/aeatbe/repository/WishlistRepository.java
+++ src/main/java/jeje/work/aeatbe/repository/WishlistRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WishlistRepository extends JpaRepository<Wishlist, Integer> {
+public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 }
