37412ead5ced63666e91c774e147a71a8b6b898f
peacefullyquietly <pjh73015@gmail.com>
2024-10-11 22:29:05 +0900

    feat(dto,entity) : User에 대한 새로운 필드 추가

--- src/main/java/jeje/work/aeatbe/dto/UserDTO.java
+++ src/main/java/jeje/work/aeatbe/dto/UserDTO.java
@@ -11,5 +11,20 @@ public class UserDTO {
     private String userId;
     private String allergies;
     private String freeFrom;
+    private String userName;
+    private String userImgUrl;
+
+    public UserDTO(Long id, String userName, String userImgUrl) {
+        this.id = id;
+        this.userName = userName;
+        this.userImgUrl = userImgUrl;
+    }
+
+    public UserDTO(Long id, String userId, String allergies, String freeFrom) {
+        this.id = id;
+        this.userId = userId;
+        this.allergies = allergies;
+        this.freeFrom = freeFrom;
+    }
 
 }
