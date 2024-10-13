88a33cd44f197bbdd7af7bd5c167b49b615d7964
youcaslte03 <luckthb325@naver.com>
2024-10-13 22:12:10 +0900

    feat:url변경

--- src/main/java/jeje/work/aeatbe/controller/ArticleLikeController.java
+++ src/main/java/jeje/work/aeatbe/controller/ArticleLikeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/columnLikes")
+@RequestMapping("/api/article/like")
 @RequiredArgsConstructor
 public class ArticleLikeController {
     private final ArticleLikeService articleLikeService;
