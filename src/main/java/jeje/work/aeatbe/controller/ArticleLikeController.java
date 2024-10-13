1c5764986a906b7035b52c1014400322f48d2d52
youcaslte03 <luckthb325@naver.com>
2024-10-13 22:21:36 +0900

    chore:url수정

--- src/main/java/jeje/work/aeatbe/controller/ArticleLikeController.java
+++ src/main/java/jeje/work/aeatbe/controller/ArticleLikeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/article/like")
+@RequestMapping("/api/article/likes")
 @RequiredArgsConstructor
 public class ArticleLikeController {
     private final ArticleLikeService articleLikeService;
