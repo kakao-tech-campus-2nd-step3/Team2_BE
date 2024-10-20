916edbfa9d4d572bb467f7a9f846d960481f2205
youcaslte03 <luckthb325@naver.com>
2024-10-21 04:54:59 +0900

    feat:JwtInterceptor

--- src/main/java/jeje/work/aeatbe/interceptor/JwtInterceptor.java
+++ src/main/java/jeje/work/aeatbe/interceptor/JwtInterceptor.java
@@ -1,5 +1,25 @@
 package jeje.work.aeatbe.interceptor;
 
-public class JwtInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jeje.work.aeatbe.utility.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
 
+@Component
+@AllArgsConstructor
+public class JwtInterceptor implements HandlerInterceptor {
+
+    private JwtUtil jwtUtil;
+
+    @Override
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) {
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+        }
+    }
 }
