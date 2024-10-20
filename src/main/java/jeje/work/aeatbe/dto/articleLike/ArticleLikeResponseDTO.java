9167824b33a33bd19e822653029a74128e127e66
youcaslte03 <luckthb325@naver.com>
2024-10-21 01:25:17 +0900

    feat: LoginUser annotation

--- src/main/java/jeje/work/aeatbe/dto/articleLike/ArticleLikeResponseDTO.java
+++ src/main/java/jeje/work/aeatbe/dto/articleLike/ArticleLikeResponseDTO.java
@@ -1,9 +1,8 @@
 package jeje.work.aeatbe.dto.articleLike;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+
 
 @Builder
 public record ArticleLikeResponseDTO(Long articleLikeId, int count) {
