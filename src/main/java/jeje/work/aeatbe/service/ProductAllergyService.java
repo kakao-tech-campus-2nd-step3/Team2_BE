8254075d6c97fb0e737a8017f591a79c16531aaa
Jeje <jjh4450jjh4450@gmail.com>
2024-10-25 06:26:57 +0900

    feat(Allergy/FreeFrom): 태그 조회 기능 추가

--- src/main/java/jeje/work/aeatbe/service/ProductAllergyService.java
+++ src/main/java/jeje/work/aeatbe/service/ProductAllergyService.java
@@ -1,10 +1,13 @@
 package jeje.work.aeatbe.service;
 
+import jeje.work.aeatbe.dto.product.ProductAllergyDTO;
 import jeje.work.aeatbe.entity.ProductAllergy;
 import jeje.work.aeatbe.exception.ProductAllergyNotFoundException;
 import jeje.work.aeatbe.mapper.product.ProductAllergyMapper;
 import jeje.work.aeatbe.repository.ProductAllergyRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 상품 알레르기 서비스 레이어
@@ -14,14 +17,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductAllergyService {
     private final ProductAllergyRepository ProductAllergyRepository;
+    private final ProductAllergyMapper ProductAllergyMapper;
 
-    public ProductAllergyService(jeje.work.aeatbe.repository.ProductAllergyRepository productAllergyRepository, jeje.work.aeatbe.mapper.product.ProductAllergyMapper productAllergyMapper) {
+    public ProductAllergyService(jeje.work.aeatbe.repository.ProductAllergyRepository productAllergyRepository, jeje.work.aeatbe.mapper.product.ProductAllergyMapper productAllergyMapper, jeje.work.aeatbe.mapper.product.ProductAllergyMapper productAllergyMapper1) {
         ProductAllergyRepository = productAllergyRepository;
+        ProductAllergyMapper = productAllergyMapper1;
     }
 
     public ProductAllergy findById(Long id) {
         return ProductAllergyRepository.findById(id).orElseThrow(
                 () -> new ProductAllergyNotFoundException("해당 id의 상품 알레르기가 존재하지 않습니다." + id)
         );
+    }
+
+    public List<ProductAllergy> getAllergyListByProductId(Long productId) {
+        return ProductAllergyRepository.findByProductId(productId);
+    }
+
+    public List<ProductAllergyDTO> getAllergyDTOList(Long productId) {
+        return getAllergyListByProductId(productId).stream()
+                .map(ProductAllergyMapper::toDTO)
+                .toList();
+    }
+
+    public List<String> getAllergyTags(Long productId) {
+        return getAllergyListByProductId(productId).stream()
+                .map(ProductAllergy::getAllergy)
+                .map(allergyCategory -> allergyCategory.getAllergyType())
+                .toList();
     }
 }
--- src/main/java/jeje/work/aeatbe/service/ProductFreeFromService.java
+++ src/main/java/jeje/work/aeatbe/service/ProductFreeFromService.java
@@ -1,10 +1,14 @@
 package jeje.work.aeatbe.service;
 
+import jeje.work.aeatbe.dto.product.ProductFreeFromDTO;
 import jeje.work.aeatbe.entity.ProductFreeFrom;
 import jeje.work.aeatbe.exception.ProductFreeFromNotFoundException;
+import jeje.work.aeatbe.mapper.BaseEntityMapper;
 import jeje.work.aeatbe.mapper.product.ProductFreeFromMapper;
 import jeje.work.aeatbe.repository.ProductFreeFromRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 상품 알레르기 서비스 레이어
@@ -13,14 +17,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductFreeFromService {
     private final ProductFreeFromRepository productFreeFromRepository;
+    private final ProductFreeFromMapper productFreeFromMapper;
 
     public ProductFreeFromService(ProductFreeFromRepository productFreeFromRepository, ProductFreeFromMapper productFreeFromMapper) {
         this.productFreeFromRepository = productFreeFromRepository;
+        this.productFreeFromMapper = productFreeFromMapper;
     }
 
     public ProductFreeFrom getById(Long id) {
         return productFreeFromRepository.findById(id).orElseThrow(
                 () -> new ProductFreeFromNotFoundException("해당 id의 상품 알레르기 정보가 없습니다. id: " + id)
         );
+    }
+
+    public List<ProductFreeFrom> getFreeFromListByProductId(Long productId) {
+        return productFreeFromRepository.findByProductId(productId);
+    }
+
+    public List<ProductFreeFromDTO> getFreeFromDTOList(Long productId) {
+        return getFreeFromListByProductId(productId).stream()
+                .map(productFreeFromMapper::toDTO)
+                .toList();
+    }
+
+    public List<String> getFreeFromTags(Long productId) {
+        return getFreeFromListByProductId(productId).stream()
+                .map(ProductFreeFrom::getFreeFromCategory)
+                .map(freeFromCategory -> freeFromCategory.getFreeFromType())
+                .toList();
     }
 }
