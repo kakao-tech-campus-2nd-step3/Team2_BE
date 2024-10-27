afdb1b61fe4a8bf8ae6a33c26312443a2e4e10ee
Jeje <jjh4450jjh4450@gmail.com>
2024-10-27 16:32:07 +0900

    feat(~~Category): 태그 기반 조회 로직 추가

--- src/main/java/jeje/work/aeatbe/repository/AllergyCategoryRepository.java
+++ src/main/java/jeje/work/aeatbe/repository/AllergyCategoryRepository.java
@@ -4,6 +4,9 @@ import jeje.work.aeatbe.entity.AllergyCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AllergyCategoryRepository extends JpaRepository<AllergyCategory, Long> {
+   Optional<AllergyCategory> findByAllergyType(String allergyType);
 }
--- src/main/java/jeje/work/aeatbe/service/AllergyCategoryService.java
+++ src/main/java/jeje/work/aeatbe/service/AllergyCategoryService.java
@@ -3,6 +3,7 @@ package jeje.work.aeatbe.service;
 import jeje.work.aeatbe.dto.AllergyCategory.AllergyCategoryDTO;
 import jeje.work.aeatbe.entity.AllergyCategory;
 import jeje.work.aeatbe.exception.AllergyCategoryNotFoundException;
+import jeje.work.aeatbe.mapper.allergyCategory.AllergyCategoryMapper;
 import jeje.work.aeatbe.repository.AllergyCategoryRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +13,16 @@ import java.util.stream.Collectors;
 /**
  * 알러지 카테고리 서비스 레이어
  *
- * @author jjh4450git@gmail.com
  */
 @Service
 public class AllergyCategoryService {
 
     private final AllergyCategoryRepository allergyCategoryRepository;
+    private final AllergyCategoryMapper allergyCategoryMapper;
 
-    public AllergyCategoryService(AllergyCategoryRepository allergyCategoryRepository) {
+    public AllergyCategoryService(AllergyCategoryRepository allergyCategoryRepository, AllergyCategoryMapper allergyCategoryMapper) {
         this.allergyCategoryRepository = allergyCategoryRepository;
+        this.allergyCategoryMapper = allergyCategoryMapper;
     }
 
     /**
@@ -29,10 +31,23 @@ public class AllergyCategoryService {
      * @param id 알러지 카테고리 id
      * @return id에 해당하는 알러지 카테고리
      */
-    public AllergyCategory findById(Long id) {
+    protected AllergyCategory findById(Long id) {
         return allergyCategoryRepository.findById(id).orElseThrow(
                 () -> new AllergyCategoryNotFoundException("해당 id의 알러지 카테고리가 존재하지 않습니다.")
         );
+    }
+
+    /**
+     * 테이블에서 태그를 기반으로 알러지 카테고리를 조회하는 메서드
+     *
+     * @param allergyType 조회할 알러지 카테고리의 이름
+     * @return 조회된 알러지 카테고리
+     * @throws AllergyCategoryNotFoundException 조회된 알러지 카테고리가 없을 경우 예외 발생
+     */
+    public AllergyCategoryDTO getProductAllergyByType(String allergyType) {
+        AllergyCategory allergyCategory = allergyCategoryRepository.findByAllergyType(allergyType)
+                .orElseThrow(() -> new AllergyCategoryNotFoundException("해당 알러지 카테고리가 존재하지 않습니다."));
+        return allergyCategoryMapper.toDTO(allergyCategory);
     }
 
     /**
