8e6096bc979fda7f1bf29b91690ee2060d497698
peacefullyquietly <pjh73015@gmail.com>
2024-11-01 22:02:44 +0900

    feat : ProductRepository에 필요한 메서드 추가

--- src/main/java/jeje/work/aeatbe/repository/ProductRepository.java
+++ src/main/java/jeje/work/aeatbe/repository/ProductRepository.java
@@ -1,9 +1,20 @@
 package jeje.work.aeatbe.repository;
 
+import java.util.List;
 import jeje.work.aeatbe.entity.Product;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByProductNameContaining(String q, Pageable pageable);
+
+    Page<Product> findByAllergy(List<String> allergies, Pageable pageable);
+
+    Page<Product> findByFreeFrom(List<String> freeFroms, Pageable pageable);
+
+    Page<Product> findByPriceBetween(int priceMin, int priceMax, Pageable pageable);
 }
