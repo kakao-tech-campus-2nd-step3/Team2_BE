f310c473f00408c593c4a98fa7f79c8987825a13
youcaslte03 <luckthb325@naver.com>
2024-11-09 03:13:56 +0900

    chore:redis 기본설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
 
 	//redis
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 }
 
 tasks.withType<Test> {
