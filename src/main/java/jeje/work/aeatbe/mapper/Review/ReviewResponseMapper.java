a625ab3917eed381e881c4a4ee5640ee65fc5c9f
jjh4450 <jjh4450git@gmail.com>
2024-11-09 02:03:11 +0900

    feat(ReviewResponseMapper): ReviewResponseMapper 구성

---
+++ src/main/java/jeje/work/aeatbe/mapper/Review/ReviewResponseMapper.java
@@ -0,0 +1,18 @@
+package jeje.work.aeatbe.mapper.Review;
+
+import jeje.work.aeatbe.dto.review.ReviewDTO;
+import jeje.work.aeatbe.dto.review.ReviewResponseDTO;
+import jeje.work.aeatbe.dto.user.UserInfoResponseDTO;
+
+public class ReviewResponseMapper {
+    public ReviewResponseDTO toDTO(ReviewDTO reviewDTO, UserInfoResponseDTO userInfoResponseDTO) {
+        return ReviewResponseDTO.builder()
+            .id(reviewDTO.id())
+            .rate(reviewDTO.rate())
+            .content(reviewDTO.content())
+            .user(userInfoResponseDTO)
+            .productId(reviewDTO.productId())
+            .productImgUrl(reviewDTO.productImgUrl())
+            .build();
+    }
+}
