344bbb3c0bbcab718b477a5445559baa1c6fb3b9
peacefullyquietly <pjh73015@gmail.com>
2024-11-09 02:01:23 +0900

    feat : haccp 서비스 호출을 위한 컨트롤러 클래스 작성

---
+++ src/main/java/jeje/work/aeatbe/controller/HaccpController.java
@@ -0,0 +1,36 @@
+package jeje.work.aeatbe.controller;
+
+import jeje.work.aeatbe.service.HaccpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+
+/**
+ * haccp api 호출용 컨트롤러
+ */
+@RestController
+@RequestMapping("/api/test")
+public class HaccpController {
+
+    private final HaccpService haccpService;
+
+    @Autowired
+    public HaccpController(HaccpService haccpService) {
+        this.haccpService = haccpService;
+    }
+
+    /**
+     * api 호출
+     */
+    @GetMapping
+    public void redirectToHaccpApi() {
+//        String apiUrl = haccpService.getProductApiUrl(); // 생성된 URL 가져오기
+//        System.out.println("Redirecting to: " + apiUrl); // 리다이렉트할 URL 로그 출력
+
+        haccpService.getProductApi();
+//        return new RedirectView(apiUrl); // 해당 URL로 리다이렉트
+    }
+}
