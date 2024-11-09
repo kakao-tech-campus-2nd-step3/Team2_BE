1c3c9988044cbdfdeeed6559b6cf0e589c552a90
humpose <yjpx416@gamil.com>
2024-11-09 23:56:24 +0900

    refactor: mapper로 dto<->entity 변환 메서드 관리

---
+++ src/main/java/jeje/work/aeatbe/mapper/article/ArticleMapper.java
@@ -0,0 +1,52 @@
+package jeje.work.aeatbe.mapper.article;
+
+import java.util.Arrays;
+import jeje.work.aeatbe.dto.article.ArticleDTO;
+import jeje.work.aeatbe.dto.article.ArticleResponseDTO;
+import jeje.work.aeatbe.entity.Article;
+import jeje.work.aeatbe.utility.ArticleUtil;
+import org.springframework.stereotype.Component;
+
+/*
+칼럼 매퍼
+ */
+@Component
+public class ArticleMapper {
+
+    /**
+     * Entity -> DTO
+     * @param article
+     * @return
+     */
+    public ArticleDTO toDTO(Article article) {
+        return ArticleDTO.builder()
+            .id(article.getId())
+            .title(article.getTitle())
+            .date(article.getDate())
+            .author(article.getAuthor())
+            .tags(article.getTags())
+            .content(article.getContent())
+            .thumbnailUrl(article.getThumbnailUrl())
+            .likes(article.getLikes())
+            .build();
+    }
+
+    /**
+     * Entity -> ResponseDTO
+     * @param article
+     * @return
+     */
+    public ArticleResponseDTO toResponseDTO(Article article) {
+        return ArticleResponseDTO.builder()
+            .id(article.getId())
+            .title(article.getTitle())
+            .imgurl(article.getThumbnailUrl())
+            .createdAt(article.getDate())
+            .auth(article.getAuthor())
+            .keyword(Arrays.asList(article.getTags().split(",")))
+            .content(ArticleUtil.extractContentList(article.getContent()))
+            .subtitle(ArticleUtil.extractSubtitle(article.getContent()))
+            .build();
+    }
+
+}
