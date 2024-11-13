95d126f65229d5827eb48d0bc1896888f85b8009
peacefullyquietly <pjh73015@gmail.com>
2024-11-14 01:57:28 +0900

    chore : 무분별한 api 호출 방지를 위해 컨트롤러 비활성화

--- src/main/java/jeje/work/aeatbe/controller/SearchShopController.java
+++ src/main/java/jeje/work/aeatbe/controller/SearchShopController.java
@@ -1,26 +1,26 @@
-package jeje.work.aeatbe.controller;
-
-import jeje.work.aeatbe.service.SearchShopService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/test2")
-public class SearchShopController {
-
-    private final SearchShopService searchShopService;
-
-    @Autowired
-    public SearchShopController(SearchShopService searchShopService) {
-        this.searchShopService = searchShopService;
-    }
-
-    @GetMapping
-    public void getSellerApi() {
-//        searchShopService.getShopApi(); // 단순 호출 테스트
-        searchShopService.fetchProductsFromApi();
-    }
-
-}
+//package jeje.work.aeatbe.controller;
+//
+//import jeje.work.aeatbe.service.SearchShopService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/test2")
+//public class SearchShopController {
+//
+//    private final SearchShopService searchShopService;
+//
+//    @Autowired
+//    public SearchShopController(SearchShopService searchShopService) {
+//        this.searchShopService = searchShopService;
+//    }
+//
+//    @GetMapping
+//    public void getSellerApi() {
+////        searchShopService.getShopApi(); // 단순 호출 테스트
+//        searchShopService.fetchProductsFromApi();
+//    }
+//
+//}
