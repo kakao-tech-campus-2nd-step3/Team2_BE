0d9ea9157f3fe283da5d831876f333b659e8418f
youcalste03 <luckthb325@naver.com>
2024-11-14 21:50:28 +0900

    refactor: myReview에서 상품명, 수정날짜 받게 설정

--- src/main/java/jeje/work/aeatbe/mapper/Review/ReviewResponseMapper.java
+++ src/main/java/jeje/work/aeatbe/mapper/Review/ReviewResponseMapper.java
@@ -1,5 +1,6 @@
 package jeje.work.aeatbe.mapper.Review;
 
+import jeje.work.aeatbe.dto.product.ProductDTO;
 import jeje.work.aeatbe.dto.review.ReviewDTO;
 import jeje.work.aeatbe.dto.review.ReviewResponseDTO;
 import jeje.work.aeatbe.dto.user.UserInfoResponseDTO;
@@ -7,14 +8,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ReviewResponseMapper {
-    public ReviewResponseDTO toDTO(ReviewDTO reviewDTO, UserInfoResponseDTO userInfoResponseDTO) {
+    public ReviewResponseDTO toDTO(ReviewDTO reviewDTO, UserInfoResponseDTO userInfoResponseDTO, ProductDTO productDto) {
         return ReviewResponseDTO.builder()
                 .id(reviewDTO.id())
                 .rate(reviewDTO.rate())
                 .content(reviewDTO.content())
                 .user(userInfoResponseDTO)
                 .productId(reviewDTO.productId())
-                .productImgUrl(reviewDTO.productImgUrl())
+                .productImgUrl(productDto.productImageUrl())
+                .date(reviewDTO.date())
                 .build();
     }
 }
