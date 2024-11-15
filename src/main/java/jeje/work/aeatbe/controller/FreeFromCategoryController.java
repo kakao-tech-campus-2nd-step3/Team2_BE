cbcea72097311f9c92c7e3b3102a68c4370d06f6
humpose <yjpx416@gamil.com>
2024-11-15 15:07:47 +0900

    feat: 모든 프리프롬 카테고리 반환

---
+++ src/main/java/jeje/work/aeatbe/controller/FreeFromCategoryController.java
@@ -0,0 +1,27 @@
+package jeje.work.aeatbe.controller;
+
+import jeje.work.aeatbe.dto.allergyCategory.AllergyCategoryDTO;
+import jeje.work.aeatbe.dto.freeFromCategory.FreeFromCategoryDTO;
+import jeje.work.aeatbe.service.AllergyCategoryService;
+import jeje.work.aeatbe.service.FreeFromCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/freefromCategories")
+public class FreeFromCategoryController {
+
+    @Autowired
+    private FreeFromCategoryService freeFromCategoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<FreeFromCategoryDTO>> all() {
+        return ResponseEntity.ok().body(freeFromCategoryService.getAllFreeFromCategory());
+    }
+}
