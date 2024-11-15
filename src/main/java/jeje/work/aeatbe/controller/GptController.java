cee132e0d785d18b60e41176b9e21b32ea302188
peacefullyquietly <pjh73015@gmail.com>
2024-11-15 15:15:09 +0900

    feat : GPTController 작성

--- src/main/java/jeje/work/aeatbe/controller/GptController.java
+++ src/main/java/jeje/work/aeatbe/controller/GptController.java
@@ -1,21 +1,35 @@
 package jeje.work.aeatbe.controller;
 
-import jeje.work.aeatbe.service.GPTApiService;
-import org.springframework.web.bind.annotation.GetMapping;
+import jeje.work.aeatbe.annotation.LoginUser;
+import jeje.work.aeatbe.dto.user.LoginUserInfo;
+import jeje.work.aeatbe.service.GptApiService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/chatGpt")
+@RequestMapping("/api/chatgpt")
 public class GptController {
-    private GPTApiService gptApiService;
+    private GptApiService gptApiService;
 
-    public GptController(GPTApiService gptApiService) {
+    public GptController(GptApiService gptApiService) {
         this.gptApiService = gptApiService;
     }
 
-    @GetMapping
-    public void testController() {
-        gptApiService.getApi("https://file3.instiz.net/data/cached_img/upload/2019/09/25/12/1533b34aa0a797a944820c3f613c63ba.jpg");
+    @PostMapping
+    public ResponseEntity<String> getCallGpt(@RequestParam("image")MultipartFile file, @LoginUser LoginUserInfo loginUserInfo) {
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().body("업로드 할 이미지를 선택해주세요");
+        }
+
+        try {
+            gptApiService.getApi(file, loginUserInfo.userId());
+            return ResponseEntity.ok("이미지 가져오기 성공");
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("이미지 가져오기 실패: " + e.getMessage());
+        }
     }
 }
